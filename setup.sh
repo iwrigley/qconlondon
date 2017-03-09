c4390eb1597d5a2d109a0aacc559b6576e483adf
Ian Wrigley <ian@variousity.com>
2017-03-09 10:13:47 +0000

    changed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install mysql-server
 sudo service mysqld start
-mysql -u root < /home/training/tutorial/mysql_scripts.sql
-sudo cp /home/training/tutorial/exercise3/solution/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar /usr/share/java/kafka-connect-jdbc
+mysql -u root < /home/training/qconlondon/mysql_scripts.sql
+sudo cp /home/training/qconlondon/exercise3/solution/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar /usr/share/java/kafka-connect-jdbc
 
